d89f57620b9e6598a3d780a7fd43a02d36b9003c
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-07-08 00:08:02 +0600

    added login validation and redirect to home page

--- src/main.js
+++ src/main.js
@@ -4,6 +4,26 @@ import "./style.css";
 
 document.getElementById("loginButton").addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("login button clicked");
-  console.log(e);
+
+  //   fixed mobile number for checking user auth
+
+  const mobileNumber = 1234567890;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  console.log(mobileNumberValueConverted, pinNumberValueConverted);
+
+  if (
+    mobileNumber === mobileNumberValueConverted &&
+    pinNumber === pinNumberValueConverted
+  ) {
+    window.location.href = "./src/pages/home.html";
+  } else {
+    alert("Invalid Crendential");
+  }
 });
