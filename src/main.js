342edd000e71015d0652c2e9789f909ca0fcab1e
KAUSAR-BD <mdkausarsah2000@gmail.com>
2026-07-07 19:44:30 +0600

    added login button click handler

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import "./style.css";
+
+// login button funtionalites
+
+document.getElementById("loginButton").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("login button clicked");
+  console.log(e);
+});
